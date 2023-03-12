5466ad557554c8f136211e8358fe327b4aff7663
Josiahfong <90824733+Josiahfong@users.noreply.github.com>
2023-03-12 19:29:03 +0800

    Add files via upload
    Code for Q1-convert CSV to JSON

---
+++ CSVJSON_FINAL.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define lenrow 2000
+
+
+int main() {
+	
+    FILE*inputCsv=fopen("C:\\Users\\Josiah\\Documents\\Book3.csv", "r");
+    FILE*output=fopen("C:\\Users\\Josiah\\Documents\\InJSON.json", "w");
+	if (inputCsv == NULL) 
+	
+	{
+        printf("Error: Unable to open CSV file.");
+        return 1;
+    }
+    printf("Test");
+	fputs("Test", output);
+    char row[lenrow];
+    char *column;
+    char *columns[100];
+    int i, num_columns, lenColumn;
+
+    char *column_names[lenrow];
+    
+    
+        if (fgets(row, lenrow, inputCsv))
+		
+		 {
+        // Parse the column names from the first row
+        column = strtok(row, ",");
+        while (column != NULL) 
+		
+		{
+            column_names[num_columns] = strdup(column);
+            fputs(column_names[num_columns], output);
+            num_columns++;
+            column = strtok(NULL, ",");
+        }
+        lenColumn=num_columns-1;
+        
+    }
+    
+    
+    
+    printf("[\n");
+    fputs("[", output);
+    
+    while (fgets(row, lenrow, inputCsv)) 
+	
+	{
+        num_columns = 0;
+        column = strtok(row, ",");
+        fputs("\n", output);
+		while (column != NULL) 
+		
+		{
+            columns[num_columns++] = column;
+            column = strtok(NULL, ",");
+        }
+        printf("  {\n");
+        fputs("  {\n", output);
+        
+		for (i = 0; i < lenColumn; i++) 
+		
+		{
+			
+            printf("    \"%s\": \"%s\"", column_names[i+1], columns[i]);
+            fputs("\n", output);
+            fputs("    ", output);
+            fputs(column_names[i+1], output);
+            fputs(":", output);
+            fputs(columns[i], output);
+            if (i < num_columns - 1)
+			
+			 {
+                printf(",");
+            }
+            
+			printf("\n");
+        }
+        
+        printf("  }\n");
+        fputs("\n  }", output);
+        
+        if (!feof(inputCsv))
+		
+		 {
+            printf(",");
+        }
+        
+		printf("\n");
+    }
+    
+    
+    printf("]\n");
+    fputs("]\n", output);
+
+    fclose(inputCsv);
+    return 0;
+}
+
