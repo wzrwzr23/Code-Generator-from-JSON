d20457374f584d06b6720ba30b98b0959d93d376
Shiyu <wangshiyu897@gmail.com>
2023-03-14 19:32:51 +0800

    upload p2 header file

---
+++ asgm2p2.h
@@ -0,0 +1,20 @@
+#ifndef ASGM2P2_H
+#define ASGM2P2_H
+using namespace std;
+
+typedef struct ClassStruct ClassStruct;
+struct ClassStruct
+{
+    string field;
+    string type;
+};
+
+string defType(string str);
+string extract(string str);
+vector<ClassStruct> unionVector(vector<ClassStruct> a, vector<ClassStruct> b);
+vector<vector<ClassStruct>> enumerationHelper(vector<ClassStruct> v);
+vector<vector<ClassStruct>> enumeration(vector<ClassStruct> v);
+void writeh(string className, vector<vector<ClassStruct>> csVecVec);
+void writecpp(string className, vector<vector<ClassStruct>> csVecVec);
+
+#endif
